ce7fe5c2d0959c60c84531833cca61bff449165e
swolepenguin <ashtonm223@gmail.com>
2020-10-22 10:18:22 -0400

    my first attempt

---
+++ checknumber.js
@@ -0,0 +1,10 @@
+function checknumber(num){
+    if ( num > 0) {
+        return 'positive';
+    }else if (num < 0) {
+        return 'negative';
+    }else (
+        return'zero';
+    )
+}
+console.log(checknumber(5));
---
+++ hello.js
@@ -0,0 +1,29 @@
+function sayhello(name) {
+    let msg = 'Hey,' + name + '. how are you?';
+    return msg;
+}
+console.log(sayhello)('SEI 1019');
+
+function sayhello(name){
+    let msg = 'hello,' + name + 'How are you?'; 
+    return msg;
+}
+console.log(sayhello)('sei 1019')
+
+function sayhello(name){
+    let msg = 'hi,' + name + 'How are you?';
+    return msg;
+}
+console.log(sayhello)('sei')
+
+function sayHello(name){
+    let msg = 'Hello,' + name + 'how are you?';
+    return msg;
+}
+console.log(sayhello)('class')
+
+function sayhello(name){
+    let msg = 'hi,' + name + 'how are you?';
+    return msg
+}
+console.log(sayhello)('classmates')
